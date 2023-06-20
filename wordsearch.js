6729e4f26d9b37705cb1950197f80f0101ac58dd
MrChan <mrkcwayne@gmail.com>
2023-06-20 01:52:03 -0600

    final

--- wordsearch.js
+++ wordsearch.js
@@ -25,3 +25,44 @@ const wordSearch = (letters, word) => {
 };
 
 module.exports = wordSearch;
+
+/** Pair programming by gckey and x33ra
+const wordSearch = (letters, word) => {
+  let result = false;
+
+  if (letters.length < 1 || word.length < 1) {
+    return false;
+  }
+  // join each row of the 2D array horizontallty to form an array of strings
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let vertical = [];
+  for (let row = 0; row < letters.length; row++) {
+    // to check for word in horizonta strings
+    for (let column; column < letters.length; column++) {
+      if (row === 0) {
+        vertical.push(letters[row][column]);
+      } else {
+        vertical[column].push(letters[row][column]);
+      }
+    }
+  
+  }
+  const verticalJoin = vertical.map(ls => ls.join(''));
+  // check for worn in vertical strings
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      result = true;
+      break;
+    }
+  }
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      result = true;
+      break;
+    }
+  }
+  return result;
+
+};
+
+module.exports = wordSearch; */
